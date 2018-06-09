fa34b1afa822d712194de64f1636c7d168723e2c
lucas.tabis <lucas.tabis@localhost.localdomain>
2018-06-09 18:53:38 +0200

    deleted prepare my repo, only usefull for epitech projects

--- prepare_my_repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-blih -u lucas.tabis@epitech.eu repository create $1
-blih -u lucas.tabis@epitech.eu repository setacl $1 ramassage-tek r
-git clone git@git.epitech.eu:/lucas.tabis@epitech.eu/$1
