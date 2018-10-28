f2244f51fe7320b3b9ce167661fcd51d9e941eaf
Lucas Tabis <lucas.tabis@epitech.eu>
2018-10-28 17:28:30 +0100

    Added branch selection.

--- push_that.sh
+++ push_that.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 read -p "Commit message : " message
+read -p "Branch : " branch
 
 git add --all
 git commit -m "$message"
-git push --force
+git push origin $branch 
 
-read -p "All changes were pushed successfully !"
+read -p "All changes were pushed successfully to origin/$branch"
