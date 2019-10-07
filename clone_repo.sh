22a901d6a0f2d1ce0575b0a451602d05fd89b2da
Lucas <lucas.tabis@epitech.eu>
2019-10-07 12:29:01 +0200

    Added -l/--list option to show current repositories.

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-read -p "Epitech repository to clone : " repo
-read -p "Epitech user to clone from : " user
+# Getting all repositories from an user
+getRepoList()
+{
+    LIST=""
 
-git clone git@git.epitech.eu:/$user@epitech.eu/$repo
+    LIST=$(blih -u $USER@epitech.eu repository list | sort) #| tr '\n' ' '
+    ARRAY=($LIST)
+    echo ${ARRAY[@]}
+    # getModuleAlias $ARRAY
+}
+
+# getModuleAlias()
+# {
+#     for it in ${ARRAY[@]}
+#     do
+	
+#     done
+# }
+
+cloneRepository()
+{
+    read -p "Epitech repository to clone : " REPO
+    git clone git@git.epitech.eu:/$0@epitech.eu/$REPO
+}
+
+getBlihUser()
+{
+    read -p "Epitech user to clone from : " USER
+
+    if [ "$1" = "-l" -o "$1" = "--list" ]
+    then
+	getRepoList USER
+    fi
+    cloneRepository $1
+}
+
+
+getBlihUser $1
