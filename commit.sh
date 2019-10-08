04f7a680c04b57b5a63beec3222c30dfac9e9866
ltabis <lucas.tabis@epitech.eu>
2019-10-08 11:24:48 +0200

    \e[92m[\e[91mfeat\e[92m] \e[33m \e[39m: \e[34mAdded commit bash file with colors.\e[39m[

---
+++ commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+doCommit()
+{
+    echo "PASSED"
+    git commit -m "\e[92m[\e[91m$TYPE\e[92m] \e[33m$TITL \e[39m: \e[34m$COMMIT\e[39m["
+}
+
+
+read -p "Type of the commit (fix/feat/minor/merge): " TYPE
+read -p "Title of the commit : " TITLE
+read -p "Message of the commit : " COMMIT
+
+for IT in 'fix' 'feat' 'minor' 'merge'
+do
+    if [ "$TYPE" = "$IT" ]
+    then
+	doCommit $TYPE $TITLE $COMMIT
+    fi
+done
