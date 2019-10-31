a60ba51421b4bf06ed3fcde191387597201b5a6d
Lucas <lucas.tabis@epitech.eu>
2019-10-31 13:06:47 +0100

    [minor] install : Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GREEN="\e[92m"
+GREEN="\e[32m"
 NORMAL="\e[39m"
 
 echo "o-----------------------------------------------------------o"
@@ -9,8 +9,10 @@ echo "|                 Created by Tabis L.                       |"
 echo "o-----------------------------------------------------------o"
 echo
 echo "This bash file will install the following programs :"
-echo -e "> $GREENclean$NORMAL : cleans temporary files."
-echo "This bash file will install the following programs :"
-echo "This bash file will install the following programs :"
-echo "This bash file will install the following programs :"
-echo "This bash file will install the following programs :"
+echo -e "> \e[32mclean$NORMAL : Cleans temporary files."
+echo -e "> \e[32mclone_repo.sh$NORMAL : Clone an Epitech repository."
+echo -e "> \e[32mcommit.sh$NORMAL : Commit to a repository."
+echo -e "> \e[32mcreate_repo.sh$NORMAL : Create a github repository."
+echo -e "> \e[32mprepare_my_repo.sh$NORMAL : Create an Epitech repository."
+echo -e "> \e[32mpush_that.sh$NORMAL : Push to a given branch."
+echo -e "> \e[32mrm_docker_images.sh$NORMAL : delete all of your docker images."
