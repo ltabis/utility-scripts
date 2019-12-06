feab9d78c74a944367ad51d23a497819b5b44921
Lucas <lucas.tabis@epitech.eu>
2019-12-06 10:14:05 +0100

    [:bug: bug fix] Fixed a bug in prepare_my_repo script.

--- bash/prepare_my_repo.sh
+++ bash/prepare_my_repo.sh
@@ -5,8 +5,9 @@ blih -u $1 repository setacl $2 ramassage-tek r
 
 git clone git@git.epitech.eu:/$1/$2
 cd $2
-cp ~/utils/.gitignore .
-git add .gitignore
+touch README.md
+git add README.md
 git commit -m "Initial commit."
 git push origin master
 
+echo "Repository created."
