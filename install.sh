d3ce78245f3a2e9e460e8a3dcb3f2460917ca716
Lucas <lucas.tabis@epitech.eu>
2019-12-18 21:27:54 +0100

    [:sparkles: new feature] UI for the install script + script installation selection.

--- install.sh
+++ install.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 
-echo "o------------------------------------------------------o"
-echo "| Welcome to the utility scripts installation software |"
-echo "|                Created by Tabis L.                   |"
-echo "o------------------------------------------------------o"
-echo
-echo "This software will install the following programs :"
-echo -e "> \e[32mclean\e[0m: Cleans temporary files."
-echo -e "> \e[32mclone_repo.sh\e[0m: Clone an Epitech repository."
-echo -e "> \e[32mcommit.py\e[0m: Commit to a repository."
-echo -e "> \e[32mcreate_repo.sh\e[0m: Create a github repository."
-echo -e "> \e[32mprepare_my_repo.sh\e[0m: Create an Epitech repository."
-echo -e "> \e[32mpush_that.sh\e[0m: Push to a given branch."
-echo -e "> \e[32mrm_docker_images.sh\e[0m: delete all of your docker images."
-echo -e "> \e[32m(WIP) update.sh\e[0m: Update the packages installed on your computer -> only works for apt."
-echo -e "> \e[32mtrash.py\e[0m: empty trash from the command line."
-echo -e "> \e[32m(WIP) integration.py\e[0m: checks your unit tests and push them on a branch if they succeeded."
+# using whiptail as the UI
+RESULTS=$(whiptail --title "Install Software" --checklist \
+"                                              o------------------------------------------------------o
+                                              | Welcome to the utility scripts installation software |
+                                              |                Created by Tabis L.                   |
+                                              o------------------------------------------------------o
+                                                            Select wich programs to install" 20 150 10 \
+"clean" "Cleans temporary files." OFF \
+"clone_repo" "Clone an Epitech repository." OFF \
+"commit" "Special format commits." OFF \
+"create_repo" "Create a github repository." OFF \
+"prepare_my_repo" "Create an Epitech repository." OFF \
+"rm_docker_images" "Delete all of your docker images." OFF \
+"update" "(WIP) Update the packages installed on your computer -> only works for apt." OFF \
+"trash" "empty trash from the command line." OFF \
+"integration" "(WIP) checks your unit tests and push them on a branch if they succeeded." OFF 3>&1 1>&2 2>&3)
 
-echo -e "\n"
-read -ep "Do you want to install them all ? Yes, there isn't any options to choose wich to install ;) (Y//N) " IN
+# Scripts paths
+SCRIPTS=(bash/clean bash/clone_repo.sh python3/commit.py bash/create_repo.sh bash/prepare_my_repo.sh bash/rm_docker_images.sh bash/update.sh python3/trash.py python3/integration.py)
+NAMES=("clean" "clone_repo" "commit" "create_repo" "prepare_my_repo" "rm_docker_images" "update" "trash" "integration")
+TOINSTALL=()
 
-if [ "$IN" != "Y" ] && [ "$IN" != "y" ]
+# Get the appropriate programs
+for SELECTED in ${RESULTS[@]}
+do
+    for i in ${!NAMES[@]}
+    do
+       if [[ "$SELECTED" == *"${NAMES[$i]}"* ]]
+        then
+            TOINSTALL=("${TOINSTALL[@]}" ${SCRIPTS[$i]})
+        fi
+    done
+done
+
+# Check if programs have been selected
+if [ ${#TOINSTALL[@]} -eq 0 ]
 then
+    echo "Nothing selected for installation, stopping ..."
     exit 0
 fi
 
-echo -e "\nCopying config files into /etc ..."
+# Copying config files
+echo -e "\nCopying config files into /etc ...\n"
 cp config/bindings.conf config/emoji.conf config/configpath.conf /etc
 
-echo -e "Creating executables ..."
-chmod 755 bash/clean
-chmod 755 bash/clone_repo.sh
-chmod 755 bash/create_repo.sh
-chmod 755 bash/prepare_my_repo.sh
-chmod 755 bash/rm_docker_images.sh
-chmod 755 bash/update.sh
+# Copying python dependancies
+cp python3/confReader.py /usr/bin/
 
-chmod 755 python3/integration.py
-chmod 755 python3/commit.py
-chmod 755 python3/trash.py
+# chmod + installation of scripts
+for PROGRAM in ${TOINSTALL[@]}
+do
+    echo -e "Copying $PROGRAM into /usr/bin ..."
+    chmod 755 $PROGRAM
+    cp $PROGRAM /usr/bin
+done
 
-echo -e "Copying scripts into /usr/bin ..."
-cp bash/clean bash/clone_repo.sh python3/integration.py python3/trash.py python3/commit.py python3/confReader.py bash/create_repo.sh bash/prepare_my_repo.sh bash/rm_docker_images.sh bash/update.sh /usr/bin
 echo -e "\nall done!"
