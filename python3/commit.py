9edb4c93ad1f2edbd884c0ec2d47656704d7cdb2
Lucas <lucas.tabis@epitech.eu>
2020-02-05 16:34:49 +0100

    :arrow_up: Added the 'fix' commit type.

--- python3/commit.py
+++ python3/commit.py
@@ -8,21 +8,28 @@ def display_help():
     print("HOW TO USE:\n")
     print("./commit.py type title description\n")
     print("-- types [Keyword -> meaning]")
-    print("   . struct ->  structure of the code")
-    print("   . perf   ->  performance improvement")
-    print("   . rm     ->  removing file(s) / code")
-    print("   . bug    ->  bug fix")
+
+    print("\nCode:")
     print("   . feat   ->  new feature")
+    print("   . fact   ->  refactoring the code")
+    print("   . perf   ->  performance improvement")
+    print("   . struct ->  structure of the code")
+
+    print("\nFixes:")
+    print("   . up     ->  general update of the code")
+    print("   . fix    ->  quick fix of the code")
+    print("   . bug    ->  bug fix")
+
+    print("\nOther:")
+    print("   . rm     ->  removing file(s) / code")
     print("   . doc    ->  documentation")
     print("   . test   ->  unit testing")
     print("   . merge  ->  merging branches")
-    print("   . fact   ->  refactoring the code")
-    print("   . up     ->  general update of the code")
 
 rEmoji = ConfigReader("/etc/emoji.conf")
 
 display_help()
-type = input("Type of the commit > ")
+type = input("\nType of the commit > ")
 description = input("description of the commit > ")
 
 tp = rEmoji.get(type)
