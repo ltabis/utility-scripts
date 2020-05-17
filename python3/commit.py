5fcf5da5d99440879e3eccb818061984bfe7daa7
Lucas <lucas.tabis@epitech.eu>
2020-05-17 12:03:48 +0200

    :arrow_up: Updated commit with new emojies!

--- python3/commit.py
+++ python3/commit.py
@@ -10,21 +10,32 @@ def display_help():
     print("-- types [Keyword -> meaning]")
 
     print("\nCode:")
+    print("   . up     ->  general update of the code")
     print("   . feat   ->  new feature")
     print("   . fact   ->  refactoring the code")
     print("   . perf   ->  performance improvement")
     print("   . struct ->  structure of the code")
-
+    
     print("\nFixes:")
-    print("   . up     ->  general update of the code")
     print("   . fix    ->  quick fix of the code")
     print("   . bug    ->  bug fix")
+    print("   . sec    ->  security fix")
+    print("   . lint   ->  removing linter warnings and errors")
+
+    print("\nProject:")
+    print("   . ci     ->  Updating/Adding CI build system")
+    print("   . depa   ->  Adding a dependency.")
+    print("   . depd   ->  Removing a dependency.")
+    print("   . conf   ->  Changing configuration files.")
+    print("   . rm     ->  removing file(s) / code")
+    print("   . mv     ->  Moving or renaming files.")
 
     print("\nOther:")
-    print("   . rm     ->  removing file(s) / code")
     print("   . doc    ->  documentation")
     print("   . test   ->  unit testing")
     print("   . merge  ->  merging branches")
+    print("   . wip    ->  work in progress")
+    print("   . new    ->  Experimenting new things")
 
 rEmoji = ConfigReader("/etc/emoji.conf")
 
